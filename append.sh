ca25855a8f05091ce5926a29c050d022adcdba60
stefen <github@stefen.ca>
2021-02-11 18:17:52 +0000

    build it in gitpod

---
+++ append.sh
@@ -0,0 +1,6 @@
+echo "\n#ifndef ZEND_ACC_CTOR" >> config.h
+echo "#define ZEND_ACC_CTOR 0" >> config.h
+echo "#endif" >> config.h
+echo "#ifndef ZEND_ACC_DTOR" >> config.h
+echo "#define ZEND_ACC_DTOR 0" >> config.h
+echo "#endif" >> config.h
---
+++ config.h
@@ -0,0 +1,6 @@
+#ifndef ZEND_ACC_CTOR
+#define ZEND_ACC_CTOR 0
+#endif
+#ifndef ZEND_ACC_DTOR
+#define ZEND_ACC_DTOR 0
+#endif
